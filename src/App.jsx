5ccb54b7e75df300e1e7ad754628ca8a13bf2c4a
GabrielDaHoraSantos <rm95298@estudante.fieb.edu.br>
2025-09-18 07:41:12 -0300

    denovo pizza

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,10 @@
-import { useState } from "react";
-import poke from './assets/pokeapi_256.png'
+const App = () => {
 
-function App() {
 
-  
-
-const Formulario = (props) => {
-
-  const [nome, setNome] = useState("Memphis")
-
-  return (
-    <div>
-      
-      
-      <input 
-      className="nome"
-      name="nome"
-      onChange={(e)=>{setNome(e.target.value)}}
-      placeholder={props.sombra != null ? props.sombra : "Texto Padrao"}
-      type="text" />
-      <button
-        className="botao"
-      onClick={()=>{alert(nome)}}>
-        Clique Here
-      </button>
-
-    </div>
-  )
-}
-
-  return (
-    <div>
-      <h3>Pizzaria 2F</h3>
-      <img src={poke} style={{widht:200, height:100}}/>
-      <Formulario sombra="Digite se nome.." />
-      <Formulario sombra="Digite se email.." />
-      <Formulario sombra="(99)99999-9999" />
-
-      <Formulario sombra="Coloque seu Cpf"/>
-      <Formulario />
-      
-      <Formulario />
-
-    </div>
-
+  return(
+    <>
+      <h1>Projeto Pizzaria</h1>
+    </>
   )
 }
 
